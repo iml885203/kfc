86565f672b8b1d3e9bd96e9d9df91af83689da9d
Logan <iml885203@gmail.com>
2026-01-30 00:22:14 +0800

    Chore: Revert dist commit for NPM publishing
    - Ignore dist/ in git (standard NPM workflow).
    - Users will install/run via 'npx @logan/kfc' from NPM registry.

--- dist/utils/cache.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getCachedDeployments(context: string, namespace: string): string[] | null;
-export declare function setCachedDeployments(context: string, namespace: string, deployments: string[]): void;
--- dist/utils/colorize.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function colorizeLogLine(line: string): string;
